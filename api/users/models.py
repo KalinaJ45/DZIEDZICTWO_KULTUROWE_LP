c1c8bd4cc913e77591f8fe36d074f43453662f3e
KalinaJ45 <kalina.juszczyk@wp.pl>
2023-01-09 19:11:49 +0100

    Update models.py

--- api/users/models.py
+++ api/users/models.py
@@ -1,6 +1,5 @@
-from distutils.command.upload import upload
 from django.db import models
-from django.contrib.auth.models import AbstractUser, BaseUserManager
+from django.contrib.auth.models import AbstractUser
 from django.utils.translation import gettext_lazy as _
 from .forest_inspectorate_dictionary import dictionary_FI
 
